56e54f260409b1a5a87cdddd43c426d9910f4cdc
m-dary-nur <m.dary.nur@gmail.com>
2020-08-05 11:45:30 +0700

    remove duplicate global style

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -57,23 +57,3 @@
 	</div>
 	<ToastPlacer />
 </div>
-
-<style>
-@media only screen and (max-width: 768px) {
-   :global(.scrolling-auto) {
-     overflow-y: auto;
-     height: calc(100vh - 105px);
-   }
-}
-
-@media only screen and (min-width: 769px) {
-   :global(.scrolling-auto) {
-     overflow-y: auto;
-     height: calc(100vh - 127px);
-   }
-}
-
-:global(.shadow-bottom-bar) {
-   box-shadow: 0px -5px 7px -7px rgba(0, 0, 0, 0.3);
-}
-</style>
