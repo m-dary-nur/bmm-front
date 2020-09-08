a24f6e284b72a9098dea11545e172089a8174413
qesya <pasquale.palena@centrica.com>
2020-09-09 00:35:53 +0700

    [purge] fix whitelist pattern

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
     content: ["./src/**/*.html", "./src/**/*.svelte"],
     whitelist: ['text-green-500'],
-    whitelistPatterns: ['/svelte-/', 'text-[^-]*', 'bg-(.*)-500'],
+whitelistPatterns: [/svelte-/, /text-[^-]*/, /bg-(.*)-500/],
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })
 
